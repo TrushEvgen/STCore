e811a892bfa76c324e37e3a1e792c23c60b0d4d7
Евгений Труш <trysh934@gmail.com>
2019-01-29 23:13:44 +0300

    Добавил модель WorkSpace

---
+++ BinaryLoad/BinaryLoad.Model/WorkSpace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryLoad.Model
+{
+    public class WorkSpace
+    {
+        public int WID { get; set; }
+
+        public int ParentWID { get; set; }
+
+        public string Name { get; set; }
+
+        public string  Description { get; set; }
+
+        public string Diagrams { get; set; }
+
+        public string Autors { get; set; }
+    }
+}
